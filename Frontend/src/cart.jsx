4b6f775f2eeed3dac31ecbf9dcfd8d4a2b2ed564
Gauri Musmade <103319755+Gaurimusmade@users.noreply.github.com>
2023-11-22 13:24:36 +0530

    Update cart.jsx

--- Frontend/src/cart.jsx
+++ Frontend/src/cart.jsx
@@ -5,8 +5,8 @@ function Cart() {
     const navigate = useNavigate();
     const [items, setItems] = useState([]);
 
-    useEffect(async() => { 
-       await fetch('https://shellcode-it.onrender.com/api/items')
+    useEffect(() => { 
+       fetch('https://shellcode-it.onrender.com/api/items')
             .then(response => response.json())
             .then(data => { 
                 console.log(data)
